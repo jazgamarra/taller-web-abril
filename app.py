3aec273122f2f58250171727ab7fcf7a66ad89dd
jazgamarra <j44min@gmail.com>
2023-04-13 17:39:47 -0400

    rick and morty api

--- app.py
+++ app.py
@@ -30,11 +30,28 @@ def rick_and_morty():
     # Hacemos el pedido a la API -- nos devuelve un diccionario
     respuesta_api = requests.get(url).json()
     
+    # Creamos variables con los datos de la respuesta 
     nombre = respuesta_api['name']
     estado = respuesta_api['status']
     url_imagen = respuesta_api['image']
 
+    # Enviamos los datos a la tarjet en html 
     return render_template('tarjeta_personaje.html', nombre=nombre, estado=estado, url_imagen=url_imagen)
+
+@app.route('/lista_de_personajes')
+def lista_de_personajes(): 
+     # Definimos la url a la que se hace el pedido 
+    # Hacemos el pedido para un solo personaje 
+    url = 'https://rickandmortyapi.com/api/character/'
+    
+    # Hacemos el pedido a la API -- nos devuelve un diccionario
+    respuesta_api = requests.get(url).json()
+    
+    lista_de_personajes = respuesta_api['results']
+    print(lista_de_personajes)
+
+    # Enviamos los datos a la tarjeta en html 
+    return render_template('lista_de_personajes.html', lista_de_personajes=lista_de_personajes )
 
 # Configuramos para correr con el boton de play
 if __name__ == '__main__': 
